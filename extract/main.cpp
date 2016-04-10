3e231f1f71ebefdd3f5cd620a1ce2be0a6170899
tangchaoliziguoguoguo <domi@mail.ustc.edu.cn>
2016-04-10 17:45:59 +0800

    stegonography

---
+++ extract/main.cpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <QtCore/QCoreApplication>
+#include <iostream>
+#include "formatextract\formatextract.cpp"
+using namespace std;
+int main(int argc, char *argv[])
+{
+	QCoreApplication a(argc, argv);
+	QString mypath("C:\\Users\\Stephen\\Desktop\\111.jpg");
+	CFormCheck AA = CFormCheck(mypath);
+	AA.ImgFormCheck("0");
+	AA.geterror();
+	return a.exec();
+}
